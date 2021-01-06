60a07206b50dab40d1e0f5c8f0145b33956376a2
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-01-06 16:03:35 +0000

    add docstrings

--- src/oiflib/core.py
+++ src/oiflib/core.py
@@ -10,7 +10,20 @@ from pyspark.sql import DataFrame
 def melt(
     df: DataFrame, id_vars: str, var_name: str = "variable", value_name: str = "value"
 ) -> DataFrame:
-    """"Unpivot a DataFrame from wide to long format"""
+    """Unpivots a DataFrame from wide to long format.
+
+    Args:
+        df: A Spark DataFrame.
+        id_vars: Column(s) to use as identifier variables.
+        var_name: Name to use for the variable column. Defaults to 'variable'.
+        value_name: Name to use for the value column. Defaults to 'value'.
+
+    Returns:
+        DataFrame: An unpivoted Spark DataFrame.
+
+    Raises:
+        Not yet implemented.
+    """
     var_columns: List[str] = [col for col in df.columns if col not in id_vars]
     expression: str = ", ".join(
         [", ".join(["'" + x + "'", "`" + x + "`"]) for x in var_columns]
--- src/oiflib/testing.py
+++ src/oiflib/testing.py
@@ -5,7 +5,7 @@ from pyspark.sql import DataFrame
 
 
 def are_dfs_equal(df1: DataFrame, df2: DataFrame) -> bool:
-    """Returns true if the schema and data of two dataframes are equal"""
+    """Returns true if the schema and data of two dataframes are equal."""
     if df1.schema != df2.schema:
         return False
     if df1.collect() != df2.collect():
