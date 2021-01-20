6ceb1e20d0171eacf04bd3caf0832ca62fed1f23
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-01-20 09:58:40 +0000

    delete get module

--- src/oiflib/air/two/get.py
+++
@@ -1,6 +0,0 @@
-df_a2_raw = pd.read_excel(
-    'http://uk-air.defra.gov.uk/reports/cat09/2006160834_DA_GHGI_1990-2018_v01-04.xlsm', 
-    sheet_name="England By Source",
-    usecols="B:AA",
-    skiprows=16
-    )
