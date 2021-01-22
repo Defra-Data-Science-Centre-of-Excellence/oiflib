126f55a610a4941429313dfa75342f012d7022c6
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-01-22 09:24:24 +0000

    rm enrich funcs from trans

--- src/oiflib/air/four/transform.py
+++ src/oiflib/air/four/transform.py
@@ -1,10 +1,1 @@
-df_a4_bounds = df_a4_raw.assign(
-    Lower_CI_bound=df_a4_raw[
-        "Annual average maximum daily 8-hour mean O3 concentration (µg/m3)"
-    ]
-    - df_a4_raw["95% confidence interval for 'All sites' (+/-)"],
-    Upper_CI_bound=df_a4_raw[
-        "Annual average maximum daily 8-hour mean O3 concentration (µg/m3)"
-    ]
-    + df_a4_raw["95% confidence interval for 'All sites' (+/-)"],
-)
+"""TODO module docstring."""
