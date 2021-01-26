aef666e28eee5ce19e8ed7a4894091d379866468
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-01-26 13:57:27 +0000

    rename variables

--- tests/air/one/test_enrich.py
+++ tests/air/one/test_enrich.py
@@ -20,8 +20,8 @@ def test_input_air_one_enrich() -> DataFrame:
     return DataFrame(
         data={
             "ShortPollName": ["NH3", "NOx", "SO2", "NMVOC", "PM2.5"] * 2,
-            "Year": [1990] * 5 + [1991] * 5,
-            "Emissions": [2] * 5 + [3] * 5,
+            "EmissionYear": [1990] * 5 + [1991] * 5,
+            "Emission": [2] * 5 + [3] * 5,
         },
     )
 
@@ -37,8 +37,8 @@ def test_expected_air_one_enrich() -> DataFrame:
     return DataFrame(
         data={
             "ShortPollName": ["NH3", "NOx", "SO2", "NMVOC", "PM2.5"] * 2,
-            "Year": [1990] * 5 + [1991] * 5,
-            "Emissions": [2] * 5 + [3] * 5,
+            "EmissionYear": [1990] * 5 + [1991] * 5,
+            "Emission": [2] * 5 + [3] * 5,
             "Index": [2 / 2 * 100] * 5 + [3 / 2 * 100] * 5,
         },
     )
