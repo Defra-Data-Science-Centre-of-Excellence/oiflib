b614f76e551358072d48b3c3c51deeda09dd52db
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-01-27 16:01:52 +0000

    release to alpha, add type hints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,16 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+from typing import Any, List
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 # -- Project information -----------------------------------------------------
 
-project = 'oiflib'
-copyright = '2021, Ed Fawcett-Taylor'
-author = 'Ed Fawcett-Taylor'
+project: str = "oiflib"
+copyright: str = "2021, Ed Fawcett-Taylor"
+author: str = "Ed Fawcett-Taylor"
 
 # The full version, including alpha/beta/rc tags
-release = 'Beta'
+release: str = "alpha"
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,34 +31,34 @@ release = 'Beta'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx'
+extensions: List[str] = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
 ]
 
 # Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
+napoleon_google_docstring: bool = True
+napoleon_numpy_docstring: bool = False
+napoleon_include_init_with_doc: bool = False
+napoleon_include_private_with_doc: bool = False
+napoleon_include_special_with_doc: bool = True
+napoleon_use_admonition_for_examples: bool = False
+napoleon_use_admonition_for_notes: bool = False
+napoleon_use_admonition_for_references: bool = False
+napoleon_use_ivar: bool = False
+napoleon_use_param: bool = True
+napoleon_use_rtype: bool = True
+napoleon_type_aliases: Any = None
+napoleon_attr_annotations: bool = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path: List[str] = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -64,9 +66,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme: str = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path: List[str] = ["_static"]
