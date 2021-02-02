e5c60c335d6b7ec08eb7b28300381bf1a852e2d5
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-02-02 18:21:09 +0000

    mv column_name_to_string to extract

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,42 +1,8 @@
 """Tests for oiflib core functions."""
+from pandas import Series
+from pandas.testing import assert_series_equal
 
-from typing import List
-
-from pandas import DataFrame, Series
-from pandas.testing import assert_frame_equal, assert_series_equal
-
-from oiflib.core import column_name_to_string, index_to_base_year
-
-
-def test_column_name_to_string():
-    """Column names have been converted to string."""
-    col1_vals: List = ["a", "b", "c"]
-    col2_vals: List = ["d", "e", "f"]
-    col3_vals: List = ["g", "h", "i"]
-
-    test_input: DataFrame = DataFrame(
-        {
-            1: col1_vals,
-            2: col2_vals,
-            3: col3_vals,
-        },
-    )
-
-    test_expected: DataFrame = DataFrame(
-        {
-            "1": col1_vals,
-            "2": col2_vals,
-            "3": col3_vals,
-        },
-    )
-
-    assert_frame_equal(
-        left=test_input.pipe(column_name_to_string),
-        right=test_expected,
-    ) and assert_frame_equal(
-        left=test_input.pipe(column_name_to_string),
-        right=test_input,
-    ) is False
+from oiflib.core import index_to_base_year
 
 
 def test_index_to_base_year():
