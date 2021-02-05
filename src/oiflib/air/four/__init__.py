2380063419808fe655edc7f15f3fe61adc767097
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-02-05 09:00:57 +0000

    import submodule functions

--- src/oiflib/air/four/__init__.py
+++ src/oiflib/air/four/__init__.py
@@ -1,1 +1,7 @@
 """Module for processing Air Four DataFrames."""
+
+from oiflib.air.four.enrich import enrich_air_four
+from oiflib.air.four.validate import (
+    validate_air_four_enriched,
+    validate_air_four_extracted,
+)
