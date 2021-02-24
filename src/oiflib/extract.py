2e070ef2567688191328c55c15a598dd0d1f8e9c
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-02-24 15:51:09 +0000

    copy latest files from EFT-Defra/issue50

--- src/oiflib/extract.py
+++ src/oiflib/extract.py
@@ -1,22 +1,138 @@
-"""."""
+"""A generic extract function.
 
-from json import load
-from typing import Dict, Union
+# TODO refactor the following as module docstring
 
+The generic :ref:`extract_function` function extracts a DataFrame for a given **theme**
+and **indicator** from an Excel or OpenDocument workbook. It uses the **theme** and
+**indicator** values to look up the location of the workbook in a JSON format metadata
+dictionary_.
+
+.. _dictionary: https://github.com/Defra-Data-Science-Centre-of-Excellence/OIF-Pipeline-Logic/blob/EFT-Defra/issue33/data/datasets.json  # noqa: B950
+
+The dictionary contains key-value pairs of parameters and arguments that are passed to
+pandas.read_excel_ method.
+
+.. _pandas.read_excel: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html
+
+For example, the dictionary contains the following information for Air One::
+
+    {
+        "air": {
+            "one": {
+                "io": "http://uk-air.defra.gov.uk/reports/cat09/2010220959_DA_API_1990-2018_V1.0.xlsx",
+                "sheet_name": "England API",
+                "usecols": "B:AA",
+                "skiprows": 13,
+                "nrows": 1602
+            },
+    ...
+        }
+    }
+
+Where:
+
+- **io** is the path to the workbook you want to extract a DataFrame from. This can be a
+  URL (as above) or a local file path.
+- **sheet_name** is the name of the sheet containing the DataFrame you want to extract.
+- **usecols** is the column range the DataFrame you want to extract.
+- **skiprows** is the number of rows (starting at 0) to skip before the header row of the
+  DataFrame you want to extract.
+- **nrows** is total number of rows to read, excluding the header row.
+
+The above is therefore the equivalent of the range ``'England API'!$B$14:$AA$1616`` in Excel
+notation.
+
+To import the extract function run:
+
+>>> from oiflib.core import extract
+
+To use it to extract the Air One DataFrame, pass it the relevant **theme** and
+**indicator** arguments as lower-case strings:
+
+>>> extracted = extract(theme="air", indicator="one")
+"""
+
+from json import load, loads
+from typing import Dict, Optional, Union
+
+from boto3 import resource
 from pandas import DataFrame, read_excel
 
+from oiflib._helper import _check_s3_or_local
 
-def _dict_from_json_file(path: str) -> Dict[str, Dict[str, Dict[str, Union[str, int]]]]:
-    """Read OIF dataset dictionary from JSON file.
+
+def _dict_from_json_local(
+    file_path: str,
+) -> Dict[str, Dict[str, Dict[str, Union[str, int]]]]:
+    """Read OIF metadata dictionary from a local JSON file.
 
     Args:
-        path (str): path to JSON file containing OIF datasets dictionary.
+        file_path (str): path to JSON file containing OIF datasets dictionary.
 
     Returns:
         Dict[str, Dict[str, Dict[str, Union[str, int]]]]: Python dict of OIF datasets.
     """
-    with open(file=path, mode="r") as file_json:
+    with open(file=file_path, mode="r") as file_json:
         dictionary: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = load(file_json)
+    return dictionary
+
+
+def _dict_from_json_s3(
+    bucket_name: Optional[str],
+    object_key: Optional[str],
+) -> Dict[str, Dict[str, Dict[str, Union[str, int]]]]:
+    """Read OIF metadata dictionary from JSON file in S3.
+
+    Args:
+        bucket_name (str): # TODO [description]
+        object_key (str): # TODO [description]
+
+    Returns:
+        Dict[str, Dict[str, Dict[str, Union[str, int]]]]: Python dict of OIF datasets.
+    """
+    s3_resource = resource("s3")
+    json_string: str = (
+        s3_resource.Bucket(bucket_name)
+        .Object(object_key)
+        .get()["Body"]
+        .read()
+        .decode("utf-8")
+    )
+    dictionary: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = loads(json_string)
+    return dictionary
+
+
+def _dict_from_json(
+    bucket_name: Optional[str],
+    object_key: Optional[str],
+    file_path: Optional[str],
+) -> Dict[str, Dict[str, Dict[str, Union[str, int]]]]:
+    """Read OIF metadata dictionary from JSON file (s3 or local).
+
+    Args:
+        bucket_name (str): # TODO [description].
+        object_key (str): # TODO [description].
+        file_path (str): path to JSON file containing OIF datasets dictionary.
+
+    Returns:
+        Dict[str, Dict[str, Dict[str, Union[str, int]]]]: Python dict of OIF datasets.
+    """
+    source: str = _check_s3_or_local(
+        bucket_name=bucket_name, object_key=object_key, file_path=file_path
+    )
+
+    dictionary: Dict[str, Dict[str, Dict[str, Union[str, int]]]]
+
+    if source == "s3":
+        dictionary = _dict_from_json_s3(
+            bucket_name=bucket_name,
+            object_key=object_key,
+        )
+    else:
+        dictionary = _dict_from_json_local(
+            file_path=str(file_path),
+        )
+
     return dictionary
 
 
@@ -66,7 +182,9 @@ def _column_name_to_string(df: DataFrame) -> DataFrame:
 def extract(
     theme: str,
     indicator: str,
-    path: str = "/home/edfawcetttaylor/repos/oiflib/data/datasets.json",
+    bucket_name: Optional[str] = "s3-ranch-019",
+    object_key: Optional[str] = "metadata_dictionary.json",
+    file_path: Optional[str] = None,
 ) -> DataFrame:
     """Reads in data for the theme and indicator specified.
 
@@ -79,16 +197,22 @@ def extract(
     column_name_to_string(). This convertion is necessary for subsequent validation.
 
     Args:
-        path (str): path to JSON file containing OIF datasets dictionary. Defaults to
-            "/home/edfawcetttaylor/repos/oiflib/data/datasets.json".
         theme (str): Theme name, as a lower case string. E.g. "air".
         indicator (str): Indicator number, as a lower case string. E.g. "one".
+        bucket_name (Optional[str], optional): [description]. Defaults to
+            "s3-ranch-019".
+        object_key (Optional[str], optional): [description]. Defaults to
+            "metadata_dictionary.json".
+        file_path (Optional[str], optional): path to JSON file containing OIF datasets
+            dictionary. Defaults to None.
 
     Returns:
         DataFrame: The DataFrame for the given theme and indicator.
     """
-    dictionary: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = _dict_from_json_file(
-        path,
+    dictionary: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = _dict_from_json(
+        bucket_name=bucket_name,
+        object_key=object_key,
+        file_path=file_path,
     )
 
     kwargs: Dict[str, Union[str, int]] = _kwargs_from_dict(
--- src/oiflib/validate.py
+++ src/oiflib/validate.py
@@ -1,13 +1,19 @@
 """Functions for validating OIF DataFrames."""
-from typing import Dict, Union
+from tempfile import NamedTemporaryFile
+from typing import Dict, Optional, Union
 
+from boto3 import resource
 from dill import load  # noqa: S403 - security warnings n/a
 from pandas import DataFrame
 from pandera import DataFrameSchema
 from pandera.errors import SchemaError
 
+from oiflib._helper import _check_s3_or_local
 
-def _dict_from_path(file_path: str) -> Dict[str, Dict[str, Dict[str, DataFrameSchema]]]:
+
+def _dict_from_pickle_local(
+    file_path: str,
+) -> Dict[str, Dict[str, Dict[str, DataFrameSchema]]]:
     """Returns dictionary of DataFrameSchema from file path.
 
     Args:
@@ -23,6 +29,73 @@ def _dict_from_path(file_path: str) -> Dict[str, Dict[str, Dict[str, DataFrameSc
         ] = load(  # noqa: S301 - security warnings n/a
             file
         )
+    return dictionary
+
+
+def _dict_from_pickle_s3(
+    bucket_name: Optional[str],
+    object_key: Optional[str],
+) -> Dict[str, Dict[str, Dict[str, Union[str, int]]]]:
+    """Read OIF metadata dictionary from JSON file in S3.
+
+    Args:
+        bucket_name (str): # TODO [description]
+        object_key (str): # TODO [description]
+
+    Returns:
+        Dict[str, Dict[str, Dict[str, Union[str, int]]]]: Python dict of OIF datasets.
+    """
+    s3_resource = resource("s3")
+
+    tmp = NamedTemporaryFile(suffix=".pkl").name
+
+    with open(tmp, "wb") as file:
+        s3_resource.Bucket(bucket_name).download_fileobj(object_key, file)
+
+    with open(tmp, "rb") as file:
+        dictionary: Dict[
+            str,
+            Dict[str, Dict[str, Union[str, int]]],
+        ] = load(  # noqa: S301 - security warnings n/a
+            file
+        )
+
+    # ? Do I need to remove the NamedTemporaryFile?
+
+    return dictionary
+
+
+def _dict_from_pickle(
+    bucket_name: Optional[str],
+    object_key: Optional[str],
+    file_path: Optional[str],
+) -> Dict[str, Dict[str, Dict[str, Union[str, int]]]]:
+    """Read OIF metadata dictionary from JSON file (s3 or local).
+
+    Args:
+        bucket_name (str): # TODO [description].
+        object_key (str): # TODO [description].
+        file_path (str): path to JSON file containing OIF datasets dictionary.
+
+    Returns:
+        Dict[str, Dict[str, Dict[str, Union[str, int]]]]: Python dict of OIF datasets.
+    """
+    source: str = _check_s3_or_local(
+        bucket_name=bucket_name, object_key=object_key, file_path=file_path
+    )
+
+    dictionary: Dict[str, Dict[str, Dict[str, Union[str, int]]]]
+
+    if source == "s3":
+        dictionary = _dict_from_pickle_s3(
+            bucket_name=bucket_name,
+            object_key=object_key,
+        )
+    else:
+        dictionary = _dict_from_pickle_local(
+            file_path=str(file_path),
+        )
+
     return dictionary
 
 
@@ -54,7 +127,9 @@ def validate(
     indicator: str,
     stage: str,
     df: DataFrame,
-    file_path: str = "/home/edfawcetttaylor/repos/oiflib/data/schema.pkl",
+    bucket_name: Optional[str] = "s3-ranch-019",
+    object_key: Optional[str] = "schemas.pkl",
+    file_path: Optional[str] = None,
 ) -> Union[DataFrame, SchemaError]:
     """Validates a DataFrame against a DataFrameSchema.
 
@@ -67,13 +142,20 @@ def validate(
         indicator (str): Indicator number, as a lower case string.
         stage (str): Stage in pipeline, as lower case string.
         df (DataFrame): A DataFrame to be validated.
-        file_path (str): Path to file containing dictionary of DataFrameSchema.
+        bucket_name (Optional[str], optional): [description]. Defaults to
+            "s3-ranch-019".
+        object_key (Optional[str], optional): [description]. Defaults to
+            "schemas.pkl".
+        file_path (Optional[str], optional): path to dill pickled file containing
+            schemas. Defaults to None.
 
     Returns:
         Union[DataFrame, SchemaError]: Either a valid DataFrame or, in the case of an
         invalid DataFrame, a SchemaError.
     """
-    dict: Dict[str, Dict[str, Dict[str, DataFrameSchema]]] = _dict_from_path(
+    dict: Dict[str, Dict[str, Dict[str, DataFrameSchema]]] = _dict_from_pickle(
+        bucket_name=bucket_name,
+        object_key=object_key,
         file_path=file_path,
     )
 
