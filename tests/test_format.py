e866f6457aca46f3b550c432346a19087b2b5b1f
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-09 17:11:46 +0000

    add tests for format module

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,1 +1,43 @@
 """Tests for format module functions."""
+
+from pandas import DataFrame
+from pandas.testing import assert_frame_equal
+from pytest_cases import fixture_ref, parametrize
+
+from oiflib.format import format
+
+
+@parametrize(
+    "additional_args, expected",
+    [
+        (
+            fixture_ref("kwargs_no_disaggregation_column"),
+            fixture_ref("expected_air_one_formatted_no_disaggregation_column"),
+        ),
+        (
+            fixture_ref("kwargs_disaggregation_column"),
+            fixture_ref("expected_air_one_formatted_disaggregation_column"),
+        ),
+        (
+            fixture_ref("kwargs_rename_disaggregation_column"),
+            fixture_ref("expected_air_one_formatted_rename_disaggregation_column"),
+        ),
+    ],
+)
+def test_format(
+    expected_air_one_enriched,
+    additional_args,
+    expected,
+) -> None:
+    """The Dataframe is formatted for OpenSDG."""
+    returned: DataFrame = format(
+        df=expected_air_one_enriched,
+        year_column="EmissionYear",
+        value_column="Index",
+        **additional_args,
+    )
+
+    assert_frame_equal(
+        left=returned,
+        right=expected,
+    )
