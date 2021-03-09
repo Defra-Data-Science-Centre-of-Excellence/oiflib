c6b3149d5e1f7c1867949b8aec38b1547f76e22b
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-09 18:15:34 +0000

    define tests for _set_data_file_name

---
+++ tests/test_push.py
@@ -0,0 +1,29 @@
+"""Tests for push module."""
+from pytest_cases import parametrize
+
+from oiflib.push import _set_data_file_name, _write_to_csv
+
+
+@parametrize(
+    "theme, indicator, expected",
+    [
+        ("air", "one", "indicator_1-1-1.csv"),
+        ("international", "four", "indicator_10-4-1.csv"),
+    ],
+)
+def test__set_data_file_name(theme, indicator, expected) -> None:
+    """A filename is returned."""
+    returned: str = _set_data_file_name(
+        theme=theme,
+        indicator=indicator,
+    )
+
+    assert returned == expected
+
+
+def test__write_to_csv(
+    expected_air_one_formatted_rename_disaggregation_column,
+) -> None:
+    """A DataFrame is written to csv."""
+    _write_to_csv()
+    pass
