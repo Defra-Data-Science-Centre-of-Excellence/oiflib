9a43a8d1a72533317c708a65501d6a9a55227683
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-09 10:49:28 +0000

    add docstring to _check_s3_or_local

--- src/oiflib/_helper.py
+++ src/oiflib/_helper.py
@@ -21,18 +21,54 @@ def _check_s3_or_local(
     object_key: Optional[str],
     file_path: Optional[str],
 ) -> str:
-    """# TODO [summary].
+    """Returns "s3", "local", or `ValueError` based on combination of `kwargs` provided.
+
+    A private function called within extract and validate. Not intended to be called
+    directly.
+
+    Examples:
+        Returns "s3" if `bucket_name` and `object_key` are provided and `file_path` is
+        `None`. For example:
+
+        >>> _check_s3_or_local(
+            bucket_name="bucket name",
+            object_key="object key",
+            file_path=None,
+        )
+        "s3"
+
+        Returns "local" if `file_path` is provided and `bucket_name` and `object_key`
+        are `None`. For example:
+
+        >>> _check_s3_or_local(
+            bucket_name=None,
+            object_key=None,
+            file_path="/path/to/file",
+        )
+        "local"
+
+        Raises a ValueError for any other combination of `kwargs`. For example:
+
+        >>> _check_s3_or_local(
+            bucket_name="bucket name",
+            object_key=None,
+            file_path="/path/to/file",
+        )
+        ValueError(
+            "You must supply either bucket_name and object_key to read from s3 or path "
+            "to read from a local file"
+        )
 
     Args:
-        bucket_name (str): # TODO [description].
-        object_key (str): # TODO [description].
-        file_path (str): # TODO [description].
+        bucket_name (str): s3 bucket name.
+        object_key (str): s3 object key.
+        file_path (str): Path to file.
 
     Raises:
-        ValueError: # TODO [description]
+        ValueError: If an invalid combination of kwargs is provided.
 
     Returns:
-        str: # TODO [description]
+        str: Either "s3" or "local".
     """
     if bucket_name and object_key and not file_path:
         return "s3"
@@ -40,6 +76,6 @@ def _check_s3_or_local(
         return "local"
     else:
         raise ValueError(
-            "You must supply either bucket_name and object_key to read from s3 or path \
-            to read from a local file"
+            "You must supply either bucket_name and object_key to read from s3 or path "
+            "to read from a local file"
         )
