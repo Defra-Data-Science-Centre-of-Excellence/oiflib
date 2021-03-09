f952fccac2ece5a6da5d67e3d1561e9893b21af0
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-09 11:12:05 +0000

    mv fixtures to conftest, add test for dict_from_s3

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,99 +1,18 @@
 """Tests for the validate module."""
-from typing import Dict
+from typing import Dict, Optional
 
-from _pytest.tmpdir import TempdirFactory
-from dill import dump  # noqa: S403 - security warnings n/a
-from numpy import float64, int64
+from dill import dumps  # noqa: S403 - security warnings n/a
 from pandas import DataFrame
 from pandas.testing import assert_frame_equal
-from pandera import Check, Column, DataFrameSchema
-from pytest import fixture
+from pandera import DataFrameSchema
 
-from oiflib.validate import _dict_from_pickle_local, _schema_from_dict, validate
-
-
-@fixture(scope="module")
-def schema_dict() -> Dict[str, Dict[str, Dict[str, DataFrameSchema]]]:
-    """A minimal schema dictionary for testing the validate module."""
-    return {
-        "test_theme": {
-            "test_indicator": {
-                "test_stage": DataFrameSchema(
-                    columns={
-                        "1": Column(
-                            pandas_dtype=str,
-                            checks=Check.isin(["a", "b"]),
-                            allow_duplicates=False,
-                        ),
-                        "2": Column(
-                            pandas_dtype=int64,
-                            checks=Check.in_range(4, 5),
-                            allow_duplicates=False,
-                        ),
-                        "3": Column(
-                            pandas_dtype=float64,
-                            checks=Check.in_range(6.0, 9.0),
-                            allow_duplicates=False,
-                        ),
-                    },
-                    coerce=True,
-                    strict=True,
-                ),
-            },
-        },
-    }
-
-
-@fixture(scope="module")
-def file_pkl(
-    tmpdir_factory: TempdirFactory,
-    schema_dict: Dict[str, Dict[str, Dict[str, DataFrameSchema]]],
-) -> str:
-    """Dill pickles dict, writes to temp file, returns path as string.
-
-    Args:
-        tmpdir_factory (TempdirFactory): A pytest pytest.fixture for creating temporary
-            directories.
-        schema_dict (Dict[str, Dict[str, Dict[str, Any]]]): The dictionary of
-            DataFrameSchema to be dill pickled and written to the temporary file.
-
-    Returns:
-        str: The path of the temporary JSON file.
-    """
-    path = tmpdir_factory.mktemp("test").join("test.pkl")
-
-    path_as_string: str = str(path)
-
-    with open(path_as_string, "wb") as file:
-        dump(schema_dict, file)
-
-    return path_as_string
-
-
-@fixture(scope="module")
-def schema() -> DataFrameSchema:
-    """A minimal schema for testing the validate module."""
-    return DataFrameSchema(
-        columns={
-            "1": Column(
-                pandas_dtype=str,
-                checks=Check.isin(["a", "b"]),
-                allow_duplicates=False,
-            ),
-            "2": Column(
-                pandas_dtype=int64,
-                checks=Check.in_range(4, 5),
-                allow_duplicates=False,
-            ),
-            "3": Column(
-                pandas_dtype=float64,
-                checks=Check.in_range(6.0, 9.0),
-                allow_duplicates=False,
-            ),
-        },
-        coerce=True,
-        strict=True,
-    )
+from oiflib.validate import (
+    _dict_from_pickle,
+    _dict_from_pickle_local,
+    _dict_from_pickle_s3,
+    _schema_from_dict,
+    validate,
+)
 
 
 def test__dict_from_pickle_local(
@@ -105,6 +24,84 @@ def test__dict_from_pickle_local(
         file_path=file_pkl,
     )
     assert _ == schema_dict
+
+
+def test__dict_from_pickle_s3(
+    test_s3_resource,
+    schema_dict: Dict[str, Dict[str, Dict[str, DataFrameSchema]]],
+    bucket_name: str = "s3-ranch-019",
+    object_key: str = "schemas.pkl",
+) -> None:
+    """Returns same dictionary."""
+    test_s3_resource.create_bucket(Bucket=bucket_name)
+
+    test_s3_object = test_s3_resource.Object(
+        bucket_name=bucket_name,
+        key=object_key,
+    )
+
+    pickled_obj: bytes = dumps(schema_dict)
+
+    test_s3_object.put(
+        Body=pickled_obj,
+    )
+
+    returned = _dict_from_pickle_s3(
+        bucket_name=bucket_name,
+        object_key=object_key,
+    )
+
+    assert returned == schema_dict
+
+
+class TestDictFromPickle(object):
+    """Tests for _dict_from_pickle."""
+
+    def test_local(
+        self,
+        file_pkl: str,
+        schema_dict: Dict[str, Dict[str, Dict[str, DataFrameSchema]]],
+        bucket_name: Optional[str] = None,
+        object_key: Optional[str] = None,
+    ) -> None:
+        """Path but no bucket or key returns dictionary."""
+        returned = _dict_from_pickle(
+            bucket_name=bucket_name,
+            object_key=object_key,
+            file_path=file_pkl,
+        )
+
+        assert returned == schema_dict
+
+    def test_s3(
+        self,
+        test_s3_resource,
+        schema_dict: Dict[str, Dict[str, Dict[str, DataFrameSchema]]],
+        bucket_name: str = "s3-ranch-019",
+        object_key: str = "schemas.pkl",
+        file_path: Optional[str] = None,
+    ) -> None:
+        """Bucket and key but no path returns dictionary."""
+        test_s3_resource.create_bucket(Bucket=bucket_name)
+
+        test_s3_object = test_s3_resource.Object(
+            bucket_name=bucket_name,
+            key=object_key,
+        )
+
+        pickled_obj: bytes = dumps(schema_dict)
+
+        test_s3_object.put(
+            Body=pickled_obj,
+        )
+
+        returned = _dict_from_pickle(
+            bucket_name=bucket_name,
+            object_key=object_key,
+            file_path=file_path,
+        )
+
+        assert returned == schema_dict
 
 
 def test__schema_from_dict(schema_dict, schema) -> None:
@@ -121,14 +118,14 @@ def test__schema_from_dict(schema_dict, schema) -> None:
 
 def test_validate(
     file_pkl: str,
-    df_output: DataFrame,
+    df_extracted_output: DataFrame,
 ) -> None:
     """Validating a valid DataFrame returns that DataFrame."""
     _ = validate(
         theme="test_theme",
         indicator="test_indicator",
         stage="test_stage",
-        df=df_output,
+        df=df_extracted_output,
         bucket_name=None,
         object_key=None,
         file_path=file_pkl,
@@ -136,5 +133,5 @@ def test_validate(
 
     assert_frame_equal(
         left=_,
-        right=df_output,
+        right=df_extracted_output,
     )
