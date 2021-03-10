713cf1d66afc6aac694069086bf1baad7909c008
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-10 11:18:34 +0000

    implement fully parametrized tests

--- tests/air/six/test_transform.py
+++ tests/air/six/test_transform.py
@@ -1,35 +1,49 @@
 """Tests for Air Six Transform module."""
+from typing import Callable, Dict
+
 from pandas.core.frame import DataFrame
 from pandas.testing import assert_frame_equal
+from pytest_cases import fixture_ref, parametrize
 
-from oiflib.air.six.transform import _unpivot, transform_air_six
+from oiflib.air.six.transform import _filter, _unpivot, transform_air_six
+
+dict_funcs: Dict[str, Callable[[DataFrame], DataFrame]] = {
+    "_filter": _filter,
+    "_unpivot": _unpivot,
+    "transform_air_six": transform_air_six,
+}
 
 
-def test__unpivot(
-    air_six_extracted: DataFrame,
-    air_six_transformed: DataFrame,
+@parametrize(
+    argnames="input, func_name, expected",
+    argvalues=[
+        (
+            fixture_ref("air_six_unpivoted"),
+            "_filter",
+            fixture_ref("air_six_transformed"),
+        ),
+        (
+            fixture_ref("air_six_extracted"),
+            "_unpivot",
+            fixture_ref("air_six_unpivoted"),
+        ),
+        (
+            fixture_ref("air_six_extracted"),
+            "transform_air_six",
+            fixture_ref("air_six_transformed"),
+        ),
+    ],
+    ids=dict_funcs.keys(),
+)
+def test_df_funcs(
+    input: DataFrame,
+    func_name: str,
+    expected: DataFrame,
 ) -> None:
-    """Air Six extarcted is unpivoted."""
-    returned: DataFrame = _unpivot(
-        df=air_six_extracted,
-    )
+    """An example input returns an expected output."""
+    returned: DataFrame = dict_funcs[func_name](input)
 
     assert_frame_equal(
         left=returned,
-        right=air_six_transformed,
-    )
-
-
-def test_transform_air_six(
-    air_six_extracted: DataFrame,
-    air_six_transformed: DataFrame,
-) -> None:
-    """Air Six extarcted is unpivoted."""
-    returned: DataFrame = transform_air_six(
-        df=air_six_extracted,
-    )
-
-    assert_frame_equal(
-        left=returned,
-        right=air_six_transformed,
+        right=expected,
     )
