9ea4f3b294e8e15530e723009f5317ef6684d769
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-16 17:08:29 +0000

    add tests for _add, _commit, _push, and publish

--- tests/test_push.py
+++ tests/test_push.py
@@ -1,12 +1,21 @@
 """Tests for push module."""
 from pathlib import Path
+from typing import Optional, Tuple, Union
 
+from git import Repo
 from pandas import read_csv
 from pandas.core.frame import DataFrame
 from pandas.testing import assert_frame_equal
 from pytest_cases import parametrize
 
-from oiflib.push import _set_data_file_name, _write_to_csv
+from oiflib.push import (
+    _add,
+    _commit,
+    _push,
+    _set_data_file_name,
+    _write_to_csv,
+    publish,
+)
 
 
 @parametrize(
@@ -14,6 +23,10 @@ from oiflib.push import _set_data_file_name, _write_to_csv
     [
         ("air", "one", "indicator_1-1-1.csv"),
         ("international", "four", "indicator_0-0-4.csv"),
+    ],
+    ids=[
+        "for air one",
+        "for international four",
     ],
 )
 def test__set_data_file_name(theme, indicator, expected) -> None:
@@ -55,3 +68,141 @@ def test__write_to_csv(
         left=written,
         right=expected_air_one_formatted_rename_disaggregation_column,
     )
+
+
+def test__add(local_git_repo: Tuple[str, str, str, Repo]) -> None:
+    """File is added to git index."""
+    _root_name: str = local_git_repo[0]
+    _repo_name: str = local_git_repo[1]
+    _folder_name: str = local_git_repo[2]
+    _test_file_name: str = "test_file.txt"
+
+    open(f"{_root_name}/{_repo_name}/{_folder_name}/{_test_file_name}", "wb").close()
+
+    _add(
+        root=_root_name,
+        repo=_repo_name,
+        data_folder=_folder_name,
+        data_file_name=_test_file_name,
+    )
+
+    _repo: Repo = local_git_repo[3]
+
+    _most_recently_added_item: str = [
+        index_item.a_path for index_item in _repo.index.diff("HEAD")
+    ].pop()
+
+    assert _most_recently_added_item == f"{_folder_name}/{_test_file_name}"
+
+
+@parametrize(
+    "data_commit_message, expected",
+    [
+        (None, "add data for test_theme test_indicator"),
+        ("initial commit", "initial commit"),
+    ],
+    ids=[
+        "with generated commit message",
+        "with provided commit message",
+    ],
+)
+def test__commit(
+    local_git_repo: Tuple[str, str, str, Repo],
+    data_commit_message: Optional[str],
+    expected: str,
+) -> None:
+    """File is committed with generated or provided message."""
+    _root_name: str = local_git_repo[0]
+    _repo_name: str = local_git_repo[1]
+    _folder_name: str = local_git_repo[2]
+    _test_file_name: str = "test_file.txt"
+
+    open(f"{_root_name}/{_repo_name}/{_folder_name}/{_test_file_name}", "wb").close()
+
+    _repo: Repo = local_git_repo[3]
+
+    _repo.index.add(f"{_folder_name}/{_test_file_name}")
+
+    _commit(
+        root=_root_name,
+        repo=_repo_name,
+        theme="test_theme",
+        indicator="test_indicator",
+        data_commit_message=data_commit_message,
+    )
+
+    _most_recent_commit_message: str = _repo.head.commit.message
+
+    assert _most_recent_commit_message == expected
+
+
+@parametrize(
+    "branches",
+    [
+        ("test"),
+        (("test", "master")),
+    ],
+    ids=[
+        "to same branch",
+        "to different branch",
+    ],
+)
+def test__push(
+    branches: Union[str, Tuple[str, str]], local_git_repo: Tuple[str, str, str, Repo]
+) -> None:
+    """File is pushed to remote."""
+    _root_name: str = local_git_repo[0]
+    _repo_name: str = local_git_repo[1]
+    _folder_name: str = local_git_repo[2]
+    _test_file_name: str = "test_file.txt"
+
+    open(f"{_root_name}/{_repo_name}/{_folder_name}/{_test_file_name}", "wb").close()
+
+    _repo: Repo = local_git_repo[3]
+
+    _repo.index.add(f"{_folder_name}/{_test_file_name}")
+    _repo.index.commit("test commit")
+
+    _push(
+        root=_root_name,
+        repo=_repo_name,
+        branches=branches,
+    )
+
+    _repo.remotes.origin.fetch()
+
+    commits_behind = _repo.iter_commits("master..origin/master")
+
+    commits_ahead = _repo.iter_commits("origin/master..master")
+
+    count = sum(1 for commits in commits_ahead) + sum(1 for commits in commits_behind)
+
+    assert count == 0
+
+
+def test_publish(
+    expected_air_one_formatted_rename_disaggregation_column: DataFrame,
+    local_git_repo: Tuple[str, str, str, Repo],
+) -> None:
+    """DataFrame is pushed to remote."""  # noqa: D403 - capitalisation is correct
+    publish(
+        df=expected_air_one_formatted_rename_disaggregation_column,
+        theme="air",
+        indicator="one",
+        branches="test",
+        root=local_git_repo[0],
+        repo=local_git_repo[1],
+        data_folder=local_git_repo[2],
+    )
+
+    _repo: Repo = local_git_repo[3]
+
+    _repo.remotes.origin.fetch()
+
+    commits_behind = _repo.iter_commits("master..origin/master")
+
+    commits_ahead = _repo.iter_commits("origin/master..master")
+
+    count = sum(1 for commits in commits_ahead) + sum(1 for commits in commits_behind)
+
+    assert count == 0
